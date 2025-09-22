f89d46bd3704bcc89788705463f9774159ad88cd
Elinor Tchamo <tchamoelii@gmail.com>
2025-09-23 00:03:25 +0200

    Feat: move the player forward and horizontaly

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public bool isAlive = true;
+    public float RunSpeed;
+    public float HorizontalSpeed;
+    public Rigidbody rb;
+    float horizontalInput;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (isAlive)
+        {
+            Vector3 forwardMove = transform.forward * RunSpeed * Time.fixedDeltaTime;
+            Vector3 horizontalMove = transform.right * horizontalInput * HorizontalSpeed * Time.fixedDeltaTime;
+            rb.MovePosition(rb.position + forwardMove + horizontalMove);
+        }
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +33,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
     }
 }
