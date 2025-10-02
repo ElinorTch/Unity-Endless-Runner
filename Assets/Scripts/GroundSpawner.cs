c254bafca12c9911bf8bb2014eac91a1c132b0ed
Elinor Tchamo <tchamoelii@gmail.com>
2025-10-02 10:39:36 +0200

    Feat: Prefab for obstacle done

--- Assets/Scripts/GroundSpawner.cs
+++ Assets/Scripts/GroundSpawner.cs
@@ -16,7 +16,7 @@ public class GroundSpawner : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < 25; i++)
         {
             SpawnTile();
         }
